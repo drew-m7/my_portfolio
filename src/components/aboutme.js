d2236bbff106eb896a85424d2e71cf67d1ce0dd0
Drew Martin <drew@Drews-MacBook-Pro-2.local>
2022-03-17 23:25:10 -0500

    fix

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -10,7 +10,7 @@ class About extends Component {
           &nbsp;&nbsp;I am Drew Martin, I grew up in the small town of Roanoke, IL and I am 22 years old. I attended Roanoke-Benson High Scool.
           I finished my associates degree in &nbsp;&nbsp;information systems in 2020 at Illinois Central College.
         
-          I will soon graduate with my bachelors degree in computer science in 2022 at Illinois State University.<br></br>
+          I will soon graduate with my bachelors degree in computer science in 2022 at Illinois State University!<br></br>
         
           &nbsp;&nbsp;Along the way, I've had some really cool opportunities to intern at <a href="https://www.cloudpointgeo.com/">Cloudpoint Geospatial</a> and <a href="https://mirus.io/">Mirus Research</a>.
         
