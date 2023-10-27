457ce1389cf9ab93faf18ecca90f6befd39c39c3
Drew Martin <55373019+drew-m7@users.noreply.github.com>
2023-10-26 19:17:52 -0500

    Update aboutme.js

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -8,28 +8,7 @@ class About extends Component {
         <h1><b>&nbsp;&nbsp;Who Am I ?</b></h1>
         <hr style={{borderTop: '3px solid #1cb0dd', width: '20%'}}/>
         <h4>
-          &nbsp;&nbsp;I am Drew Martin, I grew up in Roanoke, IL and I am 22 years old. I attended Roanoke-Benson High Scool.
-          I finished my associates degree in information systems in &nbsp;&nbsp;2020 at Illinois Central College.
-        
-          I finished my bachelors degree in computer science at Illinois State University!
-        
-          Along the way, I've had some really cool opportunities &nbsp;&nbsp;to intern at <a href="https://www.cloudpointgeo.com/">Cloudpoint Geospatial</a> and <a href="https://mirus.io/">Mirus Research</a>.
-         
-          <br></br>
-          <br></br>
-          &nbsp;&nbsp;I have had many different jobs over the years, from automotive detailing to landscaping and construction! However, my most relevant work has been at Cloutpoint &nbsp;&nbsp;Geospatial and Mirus Research.
-          <br></br>
-          <br></br>
-          &nbsp;&nbsp;At Cloudpoint Geo I worked as a GIS intern to gather data, analyze data, and improve web map applications built with arcGIS. In 2021, I began working
-          at Mirus &nbsp;&nbsp;Research, a super cool software company in Bloomington, IL, where I was able to gain some amazing experience as a developer. My focus was 
-          mainly on frontend &nbsp;&nbsp;dev, working with Vue.js, but I also occasionally worked on backend with Python. I have been able to work on developing some really cool new features for various &nbsp;&nbsp;clients,
-          track down bugs, work on accessibility of client's web applications, and gain a ton of new skills related to software development and CI/CD!  
-          <br></br>
-          <br></br>
-          &nbsp;&nbsp;I am passionate about helping people through understanding and solving problems to improve their lives and meet their needs, and am always eager to learn &nbsp;&nbsp;something new or tackle a challenging problem!
-          I am always open to new and interesting opportunities!
-
-
+          I am Drew Martin
         </h4>
       </div>
     )
